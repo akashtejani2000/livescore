e6a3c0ae6347a4910f24e9671eb90658367cb6cd
imac <akashtejani2000@gmail.com>
2021-09-14 22:13:00 +0530

    by akash -> extension add

--- lib/main.dart
+++ lib/main.dart
@@ -40,3 +40,17 @@ class MyApp extends StatelessWidget {
 //argument get
 
 // var result=Get.find<String>(tag:"tagName");
+
+/*import 'package:get/get.dart';
+
+extension $GetxExtension on GetInterface {
+  T deleteAndPut<T>(T dependency, {String? tag}) {
+    GetInstance().delete<T>(tag: tag, force: true);
+    return Get.put<T>(dependency, tag: tag);
+  }
+}
+
+String enumToString(Object o) => o.toString().split('.').last;
+
+T enumFromString<T>(String key, List<T> values) =>
+    values.firstWhere((v) => key == enumToString(v!));*/
